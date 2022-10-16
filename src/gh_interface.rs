e41af2878112f3e60e20e39978ce49b533fabb82
Nick <speedyleion@users.noreply.github.com>
2022-10-16 16:09:38 -0700

    It only compiles every 4 tries... good luck.
    Have the tests more or less working for no prs and pr text

--- src/gh_interface.rs
+++ src/gh_interface.rs
@@ -45,7 +45,7 @@ impl<C: Cmd> GhCli<C> {
 
     pub fn diff(&mut self) -> Result<String, String> {
         self.command.arg("pr");
-        // self.command.arg("pr").arg("diff");
+        self.command.arg("diff");
         self.command.stdout(Stdio::piped());
         self.command.stderr(Stdio::piped());
         let output = self
@@ -88,14 +88,16 @@ mod tests {
         let mut mock = MockC::new();
         mock.expect_arg::<&str>()
             .with(eq("pr"))
+            .times(1)
             .returning(|_| MockC::new());
         mock.expect_arg::<&str>()
             .with(eq("diff"))
+            .times(1)
             .returning(|_| MockC::new());
         // No good way to check for pipes
         mock.expect_stdout().times(1).returning(|_| MockC::new());
         mock.expect_stderr().times(1).returning(|_| MockC::new());
-        mock.expect_output().returning(|| {
+        mock.expect_output().times(1).returning(|| {
             Ok(Output {
                 status: ExitStatus::from_raw(1),
                 stdout: vec![],
@@ -106,5 +108,51 @@ mod tests {
         let mut gh = GhCli::new(mock);
         let message = gh.diff().err().unwrap();
         assert!(message.contains("no pull requests found for branch"));
+    }
+
+    #[test]
+    fn current_pr() {
+        let expected = b"
+            diff --git a/file.1 b/file.1
+            index ff02a34..7d8ab89 100644
+            --- a/file.1
+            +++ b/file.1
+            @@ -6,3 +6,6 @@ context
+             more context
+
+             even more conext
+            +new stuff
+            +some more new stuff
+            diff --git a/path_2/file.2 b/path_2/file.2
+            new file mode 100644
+            index 0000000..dafde04
+            --- /dev/null
+            +++ b/path_2/file.2
+            @@ -0,0 +1,41 @@
+            +new stuff
+            +new stuff
+        ";
+        let mut mock = MockC::new();
+        mock.expect_arg::<&str>()
+            .with(eq("pr"))
+            .times(1)
+            .returning(|_| MockC::new());
+        mock.expect_arg::<&str>()
+            .with(eq("diff"))
+            .times(1)
+            .returning(|_| MockC::new());
+        // No good way to check for pipes
+        mock.expect_stdout().times(1).returning(|_| MockC::new());
+        mock.expect_stderr().times(1).returning(|_| MockC::new());
+        mock.expect_output().times(1).returning(|| {
+            Ok(Output {
+                status: ExitStatus::from_raw(0),
+                stdout: expected.to_vec(),
+                stderr: vec![],
+            })
+        });
+        let mut gh = GhCli::new(mock);
+        let message = gh.diff().unwrap();
+        assert_eq!(message, String::from_utf8(expected.to_vec()).unwrap());
     }
 }
