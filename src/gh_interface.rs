f18e3b867da2a93d1797e678c37ef5be3f7adf52
Nick <speedyleion@users.noreply.github.com>
2022-10-15 19:25:13 -0700

    working on the mocks, not happy wiht it :(

--- src/gh_interface.rs
+++ src/gh_interface.rs
@@ -5,17 +5,49 @@
 
 //! Module for interacting with the github command line
 
-use std::process::{Command, Stdio};
+use std::process::Command;
+use std::ffi::OsStr;
+use std::io;
+use std::process::Output;
+use std::process::Stdio;
+
+
+trait Cmd {
+    fn args<I: IntoIterator<Item = S>, S: AsRef<OsStr>>(&mut self, args: I) -> &mut Self;
+    fn stdout<T: Into<Stdio>>(&mut self, cfg: T) -> &mut Self;
+    fn stderr<T: Into<Stdio>>(&mut self, cfg: T) -> &mut Self;
+    fn output(&mut self) -> io::Result<Output>;
+}
+
+impl Cmd for Command {
+    fn args<I: IntoIterator<Item = S>, S: AsRef<OsStr>>(&mut self, args: I) -> &mut Self {
+        self.args(args)
+    }
+    fn stdout<T: Into<Stdio>>(&mut self, cfg: T) -> &mut Self {
+        self.stdout(cfg)
+    }
+    fn stderr<T: Into<Stdio>>(&mut self, cfg: T) -> &mut Self {
+        self.stderr(cfg)
+    }
+    fn output(&mut self) -> io::Result<Output> {
+        self.output()
+    }
+}
 
 #[derive(Debug, Default)]
-pub struct GhCli;
+pub struct GhCli<C> {
+    command: C,
+}
 
-impl GhCli {
-    pub fn diff() -> Result<String, String> {
-        let mut cmd = Command::new("gh");
-        cmd.args(["pr", "diff"]);
-        cmd.stdout(Stdio::piped()).stderr(Stdio::piped());
-        let output = cmd
+impl<C: Cmd> GhCli <C> {
+    pub fn new(command: C) -> Self {
+        Self{ command }
+    }
+
+    pub fn diff(&mut self) -> Result<String, String> {
+        self.command.args(["pr", "diff"]);
+        self.command.stdout(Stdio::piped()).stderr(Stdio::piped());
+        let output = self.command
             .output()
             .map_err(|e| format!("Failed running gh diff: {}", e))?;
         let status = output.status;
@@ -30,11 +62,27 @@ impl GhCli {
 }
 
 mod tests {
+    use std::fmt::Error;
     use super::*;
+    use mockall::mock;
+    use std::ffi::OsStr;
+    use std::process::Output;
+    use std::process::Stdio;
+
+    mock! {
+        Command {
+            fn args<I: IntoIterator<Item = S> + 'static, S: AsRef<OsStr> + 'static>(&mut self, args: I) -> &mut Self;
+            fn stdout<T: Into<Stdio> + 'static>(&mut self, cfg: T) -> &mut Self;
+            fn stderr<T: Into<Stdio> + 'static>(&mut self, cfg: T) -> &mut Self;
+            fn output(&mut self) -> io::Result<Output>;
+        }
+    }
 
     #[test]
     fn no_current_pr() {
-        let message = GhCli::diff().err().unwrap();
+        let mut mock = MockCommand::new();
+        let mut gh = GhCli::new(mock);
+        let message = gh.diff().err().unwrap();
         assert!(message.contains("no pull requests found for branch"));
     }
 }
