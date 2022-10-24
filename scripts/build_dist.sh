511a60db925a439707373af5b1f48fff04ce78bd
Nick <speedyleion@users.noreply.github.com>
2022-10-23 19:27:18 -0700

    Fix build dist name
    Previously the build dist name was `gh-extension...` when it should have
    been `gh-difftool...`.  Fortunatly this doesn't affect the actual
    install so this is just a cosmetic issue for now

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -3,5 +3,5 @@
 # A script to compile the rust target compatible with
 # https://github.com/cli/gh-extension-precompile
 
-cargo build --release && mkdir dist && cp target/release/gh-difftool dist/gh-extension_"$1"_linux-amd64
+cargo build --release && mkdir dist && cp target/release/gh-difftool dist/gh-difftool_"$1"_linux-amd64
 
