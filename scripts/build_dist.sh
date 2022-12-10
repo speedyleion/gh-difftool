cd3aa0f94ebee85fd2f458d8a0a3cb7b23a56ffa
Nick <speedyleion@users.noreply.github.com>
2022-12-10 15:05:02 -0800

    Mac builds (#22)
    Add mac builds to CI and future generated release binaries

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -3,5 +3,17 @@
 # A script to compile the rust target compatible with
 # https://github.com/cli/gh-extension-precompile
 
-cargo build --release && mkdir dist && cp target/release/gh-difftool dist/gh-difftool_"$1"_linux-amd64
+case "$OSTYPE" in
+  linux-gnu) os="linux" ;;
+  darwin*) os="darwin" ;;
+  *) echo "Unsupported OS: $OSTYPE."; exit 1 ;;
+esac
+
+case $(uname -m) in
+  x86_64) arch="amd64" ;;
+  arm64) arch="arm64" ;;
+  *) echo "Unsupported architecture $(uname -m)."; exit 1 ;;
+esac
+
+cargo build --release && mkdir dist && cp target/release/gh-difftool dist/gh-difftool_"$1"_"$os"-"$arch"
 
