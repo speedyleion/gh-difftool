806e5bb883b6f0157f8ad5adde7f56405a70fddf
Nick <speedyleion@users.noreply.github.com>
2022-12-10 14:36:09 -0800

    It's secret!
    some test runs

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -3,5 +3,19 @@
 # A script to compile the rust target compatible with
 # https://github.com/cli/gh-extension-precompile
 
-cargo build --release && mkdir dist && cp target/release/gh-difftool dist/gh-difftool_"$1"_linux-amd64
+case "$OSTYPE" in
+  linux-gnu) os="linux" ;;
+  darwin) os="darwin" ;;
+  *) echo "Unsupported OS: $OSTYPE."; exit 1 ;;
+esac
+
+case $(uname -m) in
+  x86_64) arch="amd64" ;;
+  arm) arch="arm64" ;;
+  *) echo "Unsupported architecture $(uname -m)."; exit 1 ;;
+esac
+
+target="dist/gh-difftool_$1_$os-$arch"
+echo "Success: $target"
+#cargo build --release && mkdir dist && cp target/release/gh-difftool dist/gh-difftool_"$1"_linux-amd64
 
