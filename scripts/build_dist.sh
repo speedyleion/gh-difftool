ba6ed8a7635d9cec9cc3fe61e82211a8180c6374
Nick <speedyleion@users.noreply.github.com>
2023-10-09 20:24:51 -0700

    wrapping should work properly. Probably.
    Debugging the windows builds

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -4,8 +4,10 @@
 # https://github.com/cli/gh-extension-precompile
 
 ext=""
+echo "The OS ${OSTYPE}"
 if [[ "${OSTYPE}" == "msys" ]]; then
-    ext=".exe"
+  echo "Windows extension ${OSTYPE}"
+  ext=".exe"
 fi
 
 if [[ "${CARGO_BUILD_TARGET}" == *"android"* ]]; then
@@ -17,5 +19,6 @@ if [[ "${CARGO_BUILD_TARGET}" == *"android"* ]]; then
   export CARGO_TARGET_${UNDERSCORE_TARGET}_LINKER=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
 fi
 
+echo "The build command"
 cargo build --release && mkdir dist && cp target/${CARGO_BUILD_TARGET}/release/gh-difftool"$ext" dist/gh-difftool_"${TARGET}""$ext"
 
