468a2b02601339352badf05ee13c42afc69d7494
Nick <speedyleion@users.noreply.github.com>
2023-10-10 20:11:09 -0700

    Add back missing version to release script (#80)

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -18,5 +18,5 @@ if [[ "${CARGO_BUILD_TARGET}" == *"android"* ]]; then
   export CARGO_TARGET_${UNDERSCORE_TARGET}_LINKER=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
 fi
 
-cargo build --release && mkdir dist && cp target/${CARGO_BUILD_TARGET}/release/gh-difftool"$ext" dist/gh-difftool_"${TARGET}""$ext"
+cargo build --release && mkdir dist && cp target/${CARGO_BUILD_TARGET}/release/gh-difftool"$ext" dist/gh-difftool_"$1"_"${TARGET}""$ext"
 
