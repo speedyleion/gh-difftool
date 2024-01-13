2af8deee99a4d743e2ff502e957a9fcad3af4c6e
Nick <speedyleion@users.noreply.github.com>
2024-01-13 12:52:10 -0800

    Use gh `macos-13-arm64` runner for mac arm64 builds

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -10,14 +10,14 @@ fi
 
 rustup target add ${CARGO_BUILD_TARGET}
 
-if [[ "${CARGO_BUILD_TARGET}" == *"android"* ]]; then
-  underscore_target=$(echo "${CARGO_BUILD_TARGET}" | tr '-' '_')
-  UNDERSCORE_TARGET=$(echo "${underscore_target}" | tr '[:lower:]' '[:upper:]')
-  export CC_${underscore_target}=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
-  export AR=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
-  export RANLIB=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib
-  export CARGO_TARGET_${UNDERSCORE_TARGET}_LINKER=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
-fi
+# if [[ "${CARGO_BUILD_TARGET}" == *"android"* ]]; then
+#   underscore_target=$(echo "${CARGO_BUILD_TARGET}" | tr '-' '_')
+#   UNDERSCORE_TARGET=$(echo "${underscore_target}" | tr '[:lower:]' '[:upper:]')
+#   export CC_${underscore_target}=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
+#   export AR=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
+#   export RANLIB=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib
+#   export CARGO_TARGET_${UNDERSCORE_TARGET}_LINKER=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
+# fi
 
 cargo build --release && mkdir dist && cp target/${CARGO_BUILD_TARGET}/release/gh-difftool"$ext" dist/gh-difftool_"$1"_"${TARGET}""$ext"
 
