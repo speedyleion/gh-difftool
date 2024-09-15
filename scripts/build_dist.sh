73a1914dbd654878a90db280222871974104ad4f
Nick <speedyleion@users.noreply.github.com>
2024-09-15 13:29:08 -0700

    Update android NDK to version 26 (#206)

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 # A script to compile the rust target compatible with
 # https://github.com/cli/gh-extension-precompile
 
@@ -13,11 +13,10 @@ rustup target add ${CARGO_BUILD_TARGET}
 if [[ "${CARGO_BUILD_TARGET}" == *"android"* ]]; then
   underscore_target=$(echo "${CARGO_BUILD_TARGET}" | tr '-' '_')
   UNDERSCORE_TARGET=$(echo "${underscore_target}" | tr '[:lower:]' '[:upper:]')
-  export CC_${underscore_target}=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
-  export AR=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
-  export RANLIB=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib
-  export CARGO_TARGET_${UNDERSCORE_TARGET}_LINKER=/usr/local/lib/android/sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
+  export CC_${underscore_target}=/usr/local/lib/android/sdk/ndk/26.3.11579264/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
+  export AR=/usr/local/lib/android/sdk/ndk/26.3.11579264/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar
+  export RANLIB=/usr/local/lib/android/sdk/ndk/26.3.11579264/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ranlib
+  export CARGO_TARGET_${UNDERSCORE_TARGET}_LINKER=/usr/local/lib/android/sdk/ndk/26.3.11579264/toolchains/llvm/prebuilt/linux-x86_64/bin/${CARGO_BUILD_TARGET}21-clang
 fi
 
 cargo build --release && mkdir dist && cp target/${CARGO_BUILD_TARGET}/release/gh-difftool"$ext" dist/gh-difftool_"$1"_"${TARGET}""$ext"
-
